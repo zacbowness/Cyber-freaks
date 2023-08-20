43cf7b498befb8cb7d2a947ef9394812b89ff7cc
tj-khalid <neftloater@gmail.com>
2023-08-19 23:05:35 -0400

    dash & roll (with effects)

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,24 +10,35 @@ public class PlayerController : MonoBehaviour
     public float sprintSpeed = 1.5f;
     public float moveSmoothness = 10f;
     public float dashForce = 3f;
+    public float rollForce = 5f;
     float moveSpeed;
     Vector2 movementInput;
     Vector2 prevMotion = new(0,-1);
-    bool isMoving;
     public bool isSprinting = false;
     [Header("Other")]
-    public SwordAttack swordAttack;
-    public Rigidbody2D body;
-    public Animator animator;
-    public ParticleSystem dustParticles;
+    public GameObject dashEcho;
+    private SwordAttack swordAttack;
+    private Rigidbody2D body;
+    private Animator animator;
+    private ParticleSystem dustParticles;
+    private TrailRenderer trail;
+    private SpriteRenderer spriteRenderer;
 
     void Start(){
+        swordAttack = GetComponentInChildren<SwordAttack>();
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        dustParticles = GetComponentInChildren<ParticleSystem>();
+        trail = GetComponentInChildren<TrailRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         moveSpeed = walkSpeed;
     }
     private void FixedUpdate(){
-        
         body.velocity = Vector2.MoveTowards(body.velocity, movementInput*moveSpeed, Time.deltaTime*moveSpeed*moveSmoothness);
         Animate();
+        if(body.velocity.magnitude <= 1.5f || isSprinting){
+            trail.emitting = false;
+        }
     }
 
     private void Animate(){
@@ -54,19 +65,44 @@ public class PlayerController : MonoBehaviour
     void OnSprintEnabled(){
         moveSpeed = sprintSpeed;
         dustParticles.Play();
-
+        isSprinting = true;
     }
     void OnSprintDisabled(){
         moveSpeed = walkSpeed;
         dustParticles.Stop();
-
+        isSprinting = false;
     }
     void OnFire(){
         animator.SetTrigger("Attack");
     }
 
     void OnDash(){
-        body.velocity += movementInput*dashForce;
+        //
+        Vector2 direction = movementInput;
+        List<RaycastHit2D> results = new List<RaycastHit2D>();
+        ContactFilter2D contactFilter2D = new ContactFilter2D();
+        contactFilter2D.SetLayerMask(1);
+        
+        for (int i = 0; i < 5; i++)
+        {
+            int count = body.Cast(
+            direction,
+            contactFilter2D,
+            results,
+            dashForce/5
+            );
+            if (count == 0){
+                body.position += direction*dashForce/5;
+                GameObject echo = Instantiate(dashEcho, body.position, body.transform.rotation);
+                echo.GetComponent<SpriteRenderer>().sprite = spriteRenderer.sprite;
+                echo.GetComponent<SpriteRenderer>().color = Color.Lerp(Color.Lerp(Color.cyan, Color.magenta, .2f*i), Color.gray, .5f);
+            }
+        }
+    }
+
+    void OnRoll(){
+        trail.emitting = true;
+        body.velocity += movementInput*rollForce;
     }
 
 }
